ad1d5a8f1fecde2f8c9ee3bc1aaa37e40f036542
Tian tian Xue (iSoftStone Information) <v-tianxue@microsoft.com>
2024-06-25 17:06:41 +0800

    add copy email tips

--- js/common.js
+++ js/common.js
@@ -113,6 +113,30 @@ $(document).ready(function() {
   $(".nav__item_email").click(function(){
     var text = $(this).find('.tip').eq(0).text()
     copyText(text)
+    var spanElement = $("<span></span>");
+    spanElement.text('"mailto:' + text + '" copied to your clipboard');  // 添加文本内容
+
+    // 设置样式
+    spanElement.css({
+        "color": "#fff",
+        "background": "#000",
+        "padding": ".5em 3em",
+        "border-radius": "4px",
+        "font-size": ".8em",
+        "position": "fixed",
+        "top": "8em",
+        "left": '50%',
+        "transform": "translateX(-50%)"
+    });
+
+    // 将 span 添加到 body 中
+    $("body").append(spanElement);
+    // 使用 delay 和 fadeOut 实现延迟消失，并在动画完成后移除元素
+    spanElement.delay(1500).fadeOut("slow", function() {
+      $(this).remove(); // 在动画完成后移除元素
+  });
+    
+
   })
 
   function copyText(text){
@@ -148,9 +172,6 @@ $(document).ready(function() {
       $(".body-box").css("opacity",1)
       $(".body-box").show()
       sessionStorage.setItem("FrontToken", passwordValue);
-      // console.log(this.$route.query.redirect);
-      let nextPath = sessionStorage.getItem("nextPath")
-      // router.push("/");
     }
   });
 
