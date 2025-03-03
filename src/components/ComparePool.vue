dd62115d202c5af754e546a28e6f84d8055c659c
Tian tian Xue (iSoftStone Information) <v-tianxue@microsoft.com>
2025-03-03 16:24:51 +0800

    comparison pool可点击选择model

--- src/components/ComparePool.vue
+++ src/components/ComparePool.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="title-content">
     <h2>
-      Compare Pool ({{ checkedModelDetailList.length }})
+      Compare Pool ({{ props.compareArr.length }})
       <SvgIcon
         @click="hideComparePool"
         class="compare-hide-icon"
@@ -15,7 +15,7 @@
       <ul>
         <li
           class="model-li"
-          v-for="(item, index) in checkedModelDetailList"
+          v-for="(item, index) in props.compareArr"
           :key="index"
           :style="{ 'border-color': colorList[index] }"
         >
@@ -41,20 +41,54 @@
         </li>
         <li
           class="model-li add-model"
-          v-for="item in 5 - checkedModelDetailList.length"
+          v-for="item in 5 - props.compareArr.length"
           :key="item"
           :style="{ 'border-color': item.color }"
+          v-popover="popoverRef"
+          @click.stop="visible = true"
         >
           <!-- <img src="@/assets/images/add-model.svg" alt="add-model" /> -->
           <SvgIcon class="add-model-svg" name="add-model"></SvgIcon>
           <p>Add</p>
         </li>
+        <el-popover
+          ref="popoverRef"
+          :visible="visible"
+          placement="top"
+          :width="576"
+          trigger="click"
+          virtual-triggering
+          persistent
+          :reference="currentLi"
+        >
+          <div id="popoverId2">
+            <el-checkbox-group v-model="checkedModelNameList" :max="5">
+              <el-checkbox
+                v-for="model in modelNameList"
+                :key="model"
+                :value="model"
+              >
+                {{ model }}
+              </el-checkbox>
+            </el-checkbox-group>
+            <div
+              style="
+                display: flex;
+                justify-content: flex-end;
+                margin-top: 20px;
+                padding-right: 20px;
+              "
+            >
+              <button @click="submit">Submit</button>
+            </div>
+          </div>
+        </el-popover>
 
         <li class="btn-box">
           <el-button
-            :color="checkedModelDetailList.length < 2 ? '#C2C2C2' : '#1093FF'"
+            :color="props.compareArr.length < 2 ? '#C2C2C2' : '#1093FF'"
             @click="compareNow"
-            :disabled="checkedModelDetailList.length < 2"
+            :disabled="props.compareArr.length < 2"
             class="compare-btn"
             >compare now</el-button
           >
@@ -70,14 +104,29 @@
 
 
 <script setup>
-import { ref, reactive, defineExpose, defineEmits } from "vue";
+import {
+  ref,
+  reactive,
+  defineExpose,
+  defineEmits,
+  onMounted,
+  watchEffect,
+} from "vue";
+import axios from "axios";
+
 import GlobalData from "@/utils/common-data";
+
+const props = defineProps({
+  compareArr: Array,
+});
+const popoverRef = ref(null);
+const visible = ref(false);
+const modelNameList = ref([]);
+const checkedModelNameList = ref([]);
 
 const checkedModelDetailList = ref([]);
 const showDetail = ref(true);
-const setCheckedModelDetailList = (arr) => {
-  checkedModelDetailList.value = arr;
-};
+
 const closeModel = (model) => {
   emit("closeModel", model);
 };
@@ -94,16 +143,60 @@ const hideComparePool = () => {
 };
 
 const emit = defineEmits([
+  "comparisonPoolSubmit",
   "removeAll",
   "compareNow",
   "closeModel",
   "hideComparePool",
 ]);
-defineExpose({
-  setCheckedModelDetailList,
-});
 
 const colorList = ref(GlobalData.colorList);
+const getAxiosData = (url) => {
+  return axios.get(url);
+};
+
+onMounted(() => {
+  fetchData();
+  document.addEventListener("click", handleClickOutside);
+});
+const handleClickOutside = (event) => {
+  // 使用 Popper 的 getPopperElement 方法获取真正的弹窗元素
+  const popperElement = document.getElementById("popoverId2");
+  // 判断点击的是否为popover外部，关闭popover
+  if (
+    popoverRef.value &&
+    popperElement &&
+    !popperElement.contains(event.target)
+  ) {
+    visible.value = false;
+  }
+};
+
+// 监听 checkedModelDetailList 的变化
+watchEffect(() => {
+  console.log("newValue", props.compareArr);
+  checkedModelNameList.value = props.compareArr.map((item) => item.modelName);
+});
+
+const submit = () => {
+  visible.value = false;
+  emit("comparisonPoolSubmit", checkedModelNameList.value);
+};
+
+const fetchData = async () => {
+  try {
+    axios.all([getAxiosData("./data/models_info.json")]).then(
+      axios.spread(function (modelInfos) {
+        const modelInfo_list = modelInfos.data.data.map((item) => {
+          return item.model;
+        });
+        modelNameList.value = modelInfo_list;
+      })
+    );
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+};
 </script>
 
 
@@ -136,8 +229,30 @@ const colorList = ref(GlobalData.colorList);
     font-size: 2em;
     font-weight: 600;
     line-height: 1.3125em;
-    padding: .2em 0;
+    padding: 0.2em 0;
   }
-  
+}
+
+:deep(.el-checkbox__label) {
+  width: 145px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  line-height: 1.4;
+}
+:deep(.el-checkbox) {
+  --el-checkbox-text-color: var(--text-color);
+  margin-right: 1em;
+}
+
+:deep(.el-tabs__header) {
+  --el-font-size-base: 1.25em;
+  .el-tabs__item {
+    padding: 0;
+    color: var(--sub-text-color);
+    &.is-active {
+      color: rgba(16, 147, 255, 1);
+    }
+  }
 }
 </style>
