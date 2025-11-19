85f7a57bd2da3d565fa60f6a2f757df6260384a7
Tian tian Xue (iSoftStone Information) <v-tianxue@microsoft.com>
2025-11-19 14:22:27 +0800

    update page

--- assets/index-CViCwiOQ.js
+++
@@ -1,1 +0,0 @@
-import{x as e,bP as o,ab as u}from"./index-BQRvI1eS.js";const i=["class","style"],E=/^on[A-Z]/,x=(a={})=>{const{excludeListeners:c=!1,excludeKeys:t}=a,l=e(()=>((t==null?void 0:t.value)||[]).concat(i)),s=u();return s?e(()=>{var n;return o(Object.entries((n=s.proxy)==null?void 0:n.$attrs).filter(([r])=>!l.value.includes(r)&&!(c&&E.test(r))))}):e(()=>({}))};export{x as u};
