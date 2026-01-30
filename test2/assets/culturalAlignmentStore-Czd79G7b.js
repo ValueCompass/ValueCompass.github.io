c88b5dace500283185e144188b0ace868c3d3860
Tian tian Xue (iSoftStone Information) <v-tianxue@microsoft.com>
2026-01-30 17:19:21 +0800

    fix bug: annocation edit error

--- test2/assets/culturalAlignmentStore-Czd79G7b.js
+++
@@ -1,1 +0,0 @@
-import{an as e}from"./index-CMDahxng.js";const n=e("culturalAlignmentStore",{state:()=>({question_info_data:null,answer_info_data:null,isTopicSelectionPageUpdateData:!0,isComparesionPageUpdateData:!0,isArenaPageUpdateData:!0,currentQuestion:null}),actions:{setQuestion(t){this.currentQuestion=t},clearQuestion(){this.currentQuestion=null}}});export{n as u};
