36cbe09a904ca9c42e47958ceba1f23b0dbe0cdf
Tiantian Xue (iSoftStone Information) <v-tiantxue@microsoft.com>
2026-06-18 12:30:53 +0800

    update page

--- test2/assets/culturalAlignmentStore-BaXjNEja.js
+++
@@ -1,1 +0,0 @@
-import{aq as e}from"./index-BU1Knl9S.js";const n=e("culturalAlignmentStore",{state:()=>({question_info_data:null,answer_info_data:null,isTopicSelectionPageUpdateData:!0,isComparesionPageUpdateData:!0,isArenaPageUpdateData:!0,currentQuestion:null}),actions:{setQuestion(t){this.currentQuestion=t},clearQuestion(){this.currentQuestion=null}}});export{n as u};
